568082da64fb3aa1ea3013d781af6e809ba1bc33
Ray Hsu <rayhsu02@yahoo.com>
2021-02-05 20:33:35 +0800

    init check in

--- Roshambo/ViewController.swift
+++ Roshambo/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var paperbutton: UIButton!
+    @IBOutlet weak var rockbutton: UIButton!
+    @IBOutlet weak var scissorbutton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rockbutton_press(_ sender: Any) {
+        let nextController = ResultViewController()
+        present(nextController, animated: true, completion: nil)
+        
+    }
+    
 }
 
