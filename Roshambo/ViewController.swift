7e01472ead151cccf81d317c3b3a9b1df126516c
Ray Hsu <rayhsu02@yahoo.com>
2021-02-07 17:54:45 +0800

    more changes

--- Roshambo/ViewController.swift
+++ Roshambo/ViewController.swift
@@ -22,23 +22,28 @@ class ViewController: UIViewController {
         return Int(value)
     }
 
-    @IBAction func rockbutton_press(_ sender: Any) {
+    @IBAction func rockbutton_press(_ sender: UIButton) {
         let nextController = storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
-        nextController.imageIdx = randomValue()
+        nextController.userchoise = getUserShape(sender: sender)
         present(nextController, animated: true, completion: nil)
         
     }
     
-    @IBAction func paperbutton_press(_ sender: Any) {
+    @IBAction func paperbutton_press(_ sender: UIButton) {
         
-        performSegue(withIdentifier: "viewResult", sender: self)
+        performSegue(withIdentifier: "viewResult", sender: sender)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier == "viewResult"){
             let controller = segue.destination as! ResultViewController
-            controller.imageIdx = randomValue()
+            controller.userchoise = getUserShape(sender: sender as! UIButton)
         }
+    }
+    
+    func getUserShape(sender: UIButton) -> Shape{
+        let title = sender.title(for: UIControl.State())!
+        return Shape(rawValue: title)!
     }
     
 }
